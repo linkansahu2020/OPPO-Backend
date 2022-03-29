20b2b23b18f309aff2311b6b8dc058fff505758f
Linkan Kumar Sahu <linkansahu2020@gmail.com>
2022-03-29 16:36:45 +0530

    product added by category

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,6 +10,14 @@ router.get('',async(req,res)=>{
         return res.status(501).send({Error:err.message})
     }
 })
+router.get('/:category',async(req,res)=>{
+    try{
+        const products = await Product.find({category:req.params.category}).lean().exec()
+        return res.status(201).send(products)
+    } catch(err){
+        return res.status(501).send({Error:err.message})
+    }
+})
 router.post('',async(req,res)=>{
     try{
         const products = await Product.create(req.body);
