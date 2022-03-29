567753bcd64ea3e5a676e626120b7e4f6e40af92
Linkan Kumar Sahu <linkansahu2020@gmail.com>
2022-03-29 22:56:45 +0530

    dotenv added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const connect = require('./configs/db')
 require('dotenv').config();
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const productsController = require('./controllers/product.controller')
 const usersController = require('./controllers/user.controller')
