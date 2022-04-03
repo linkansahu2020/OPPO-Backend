0e4f08e224f0c1cba49dd38f75c0e5ceb9df7067
Linkan Kumar Sahu <linkansahu2020@gmail.com>
2022-04-03 18:31:31 +0530

    address route

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -3,9 +3,25 @@ const express = require('express')
 const router = express.Router()
 const Address = require('../models/address.model')
 
-router.get('/',async(req,res)=>{
+router.get('/:id',async(req,res)=>{
     try{
-        const address = await Address.find().lean().exec();
+        const address = await Address.find({user_id:req.params.id}).lean().exec();
+        return res.status(201).send(address)
+    } catch(err){
+        return res.status(501).send({Error:err.message})
+    }
+})
+router.get('/one/:id',async(req,res)=>{
+    try{
+        const address = await Address.findById(req.params.id).lean().exec();
+        return res.status(201).send(address)
+    } catch(err){
+        return res.status(501).send({Error:err.message})
+    }
+})
+router.post('/',async(req,res)=>{
+    try{
+        const address = await Address.create(req.body);
         return res.status(201).send(address)
     } catch(err){
         return res.status(501).send({Error:err.message})
--- src/models/address.model.js
+++ src/models/address.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 const addressSchema = new mongoose.Schema(
     {
-        name: {type:String,required:true},
-        mobile: {type:Number,required:true},
-        house_no: {type:Number,required:true},
-        building_no: {type:String,required:false},
-        pin: {type:Number, required:true},
-        city: {type:String, required:true},
-        state: {type:String, required:true},
+        Name: {type:String,required:true},
+        Phonenumber: {type:String,required:true},
+        HouseNumber: {type:String,required:true},
+        AreaNo: {type:String,required:false},
+        Pin: {type:String, required:true},
+        City: {type:String, required:true},
+        State: {type:String, required:true},
         user_id:{
             type: mongoose.Schema.Types.ObjectId,
             ref:'users',
